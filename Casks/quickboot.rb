4dbe7ac6fdc1adfa992defebc891a6f226595870
Thurston Sandberg <thurstonsand@gmail.com>
2014-05-22 10:12:10 -0400

    Add Quickboot.app v1.1-92
    Quickboot is similar to bootchamp but only temporarily boots to chosen
    disk

---
+++ Casks/quickboot.rb
@@ -0,0 +1,7 @@
+class Quickboot < Cask
+  url 'http://buttered-cat.com/downloads/get/4/QuickBoot-1.1-92.zip'
+  homepage 'http://buttered-cat.com/quickboot'
+  version '1.1-92'
+  sha256 '5119e1113949baae165efc726b757d58a887743f1dc6ae8d03a978be7b5da4a3'
+  link 'Quickboot.app'
+end
